7c9d1ea7529a7d95cb874343c25f2a6863962f42
brol <brol31@gmail.com>
2016-03-12 19:54:36 +0100

    1.3
    voir changelog

--- menu-no.css
+++ menu-no.css
@@ -2,7 +2,7 @@
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of Balcony, a theme for Dotclear.
 #
-# Copyright (c) 2010 - 2015 annso (contact@as-i-am.fr) and contributors
+# Copyright (c) 2010 - 2016 annso (contact@as-i-am.fr) and contributors
 #
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
